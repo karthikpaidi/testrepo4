e4c0f404434aa14941c321daf7a303a27e59a7ce
karthikpaidi <karthik.paidi@gmail.com>
2016-06-27 23:51:41 -0700

    pipeline java code

---
+++ LogFileTest.java
@@ -0,0 +1,23 @@
+//this java code will create a html file and append hello world string to it
+import static java.nio.file.StandardOpenOption.*;
+import java.nio.file.*;
+import java.io.*;
+
+public class LogFileTest {
+
+  public static void main(String[] args) {
+
+    // Convert the string to a
+    // byte array.
+    String s = "Hello World!pipeline display ";
+    byte data[] = s.getBytes();
+    Path p = Paths.get("./karthikpaidi.html");
+
+    try (OutputStream out = new BufferedOutputStream(
+      Files.newOutputStream(p, CREATE, APPEND))) {
+      out.write(data, 0, data.length);
+    } catch (IOException x) {
+      System.err.println(x);
+    }
+  }
+}
